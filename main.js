f05bad175d399dfaae859fb78801d59af4c75d06
In Seop Hwang <inseop@ins-macbook-pro.lan>
2019-07-23 12:19:48 -0400

    saving progress

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-const classroom = ['Colin', 'Mesuara'];
+const classroom = ['Colin', 'Mesuara', 'Ryan', 'Chris', 'John', 'Mike', 'Nick', 'Emma', 'Olgy', 'Drew','Jay'];
 
 console.log('# Challenge #1');
 
 // Manually add every student BUT yourself to the above array by directly writing the strings in there, and console out how many are in there, using `length`.
-
+console.log(classroom.length);
 
 
 console.log('# Challenge #2');
 
 // Now use `push` to get yourself in there, and console out the new length.
 
+classroom.push('In Seop')
 
+console.log(classroom.length)
 
 console.log('# Challenge #3');
 
 // Now console out the 1st person. It should be 'Colin'!
-
+console.log(classroom[0])
 
 
 console.log('# Challenge #4');
 
 // Console out the 5th person. Don't forget to start counting at 0!
 
+console.log(classroom[4])
 
 console.log('# Challenge #5');
 
 // Now console out the last, but use `length` to find it. It should be yourself!
-
+console.log(classroom.length);
+console.log(classroom[11]);
 
 
 console.log('# Challenge #6');
@@ -34,30 +38,34 @@ console.log('# Challenge #6');
 // Now console out the next to last, still using `length`.
 
 
-
+console.log(classroom[10])
 console.log('# Challenge #7');
 
 // Remove the last person and console out the new length of the array. It should be the same as Challenge #1, since we've added then removed one person.
+const removedPerson = classroom.pop()
 
-
+console.log(classroom.length)
 
 console.log('# Challenge #8');
 
 // Now console out the person you just removed. You might have to change the code from a previous challenge to save that person before they're gone!
 
-
+console.log(removedPerson)
 
 console.log('# Challenge #9');
 
 // Now remove and console out the name of the _first_ person on the list.
-
+const oldFirst = classroom.shift();
+console.log(oldFirst);
 
 
 console.log('# Challenge #10');
 
 // Now remove and console out the name of the _new_ first person on the list.
 
-
+const newFirst = classroom[0];
+classroom.shift();
+console.log(newFirst);
 
 console.log('# Challenge #11');
 
